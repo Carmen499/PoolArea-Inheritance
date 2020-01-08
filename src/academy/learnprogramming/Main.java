7c393ed3c154d705b3594912ea2620c39ac7c851
Carmen Smith <Carmen.smith490@gmail.com>
2020-01-08 15:40:29 -0500

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Cuboid cuboid = new Cuboid(100.00, 7.00,6.00);
+
+        System.out.println("The total volume of the pool is " + cuboid.getVolume());
+    }
+}
